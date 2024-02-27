9d20164bdf37690945422bae4269aa6e6ee62a9a
Sahitya Kumar Choubey <sahityakumarchoubey@gmail.com>
2024-02-27 17:40:01 +0530

    add error handling

--- app/page.js
+++ app/page.js
@@ -14,10 +14,16 @@ export default function Home() {
   });
   const [enteredInput, setEnteredInput] = useState("");
   const [search, initiateSearch] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [found, setFound] = useState(true);
   const NEXT_PUBLIC_KEY = process.env.NEXT_PUBLIC_KEY;
 
   useEffect(() => {
     if (search) {
+      setFound(true);
+      setLoading(true);
+      setError("");
       const query = `${selectedValue.value}=${enteredInput.toUpperCase()}`;
       const fetchData = async () => {
         try {
@@ -32,15 +38,24 @@ export default function Home() {
           );
 
           if (!response.ok) {
-            throw new Error("Error fetching data");
+            throw new Error(`Error ${response.status}: ${response.statusText}`);
           }
 
           const data = await response.json();
-          console.log(data);
+
+          if (data.error) {
+            throw new Error(data.error.message);
+          }
+
           setData(data);
+          if (data.length == 0) {
+            setFound(false);
+          }
         } catch (error) {
+          setError(error.message);
           console.error("Error:", error);
         }
+        setLoading(false);
       };
 
       fetchData();
@@ -51,12 +66,18 @@ export default function Home() {
     <>
       <div className={styles.mainContainer}>
         <Navbar></Navbar>
-        <div>Hello</div>
         <SearchTab
           setSelectedValue={setSelectedValue}
           setEnteredInput={setEnteredInput}
           initiateSearch={initiateSearch}
         ></SearchTab>
+        {loading && <div className={styles.loader}></div>}
+        {error == "" ? (
+          ""
+        ) : (
+          <p className={styles.error}>An Error Occured While Fetching Data: {error}</p>
+        )}
+        {found ? "" : <p className={styles.error2}>No Data Found</p>}
         <div className={styles.dataContainer}>
           {data.map((value, index) => {
             return (
