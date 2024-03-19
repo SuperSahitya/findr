988915d39309120ccf88dfa0c5b512df1264638c
Sahitya Kumar Choubey <sahityakumarchoubey@gmail.com>
2024-03-19 21:14:21 +0530

    fix server action

--- app/api/getData.js
+++ app/api/getData.js
@@ -1,12 +1,16 @@
+"use server";
 export default async function handler(req, res) {
   const API_KEY = process.env.API_KEY;
   const query = req.query.query;
-  const response = await fetch(`https://stalker-api.vercel.app/api/connectToDatabase/?${query}`, {
-    method: "GET",
-    headers: {
-      "api-key": API_KEY,
-    },
-  });
+  const response = await fetch(
+    `https://stalker-api.vercel.app/api/connectToDatabase/?${query}`,
+    {
+      method: "GET",
+      headers: {
+        "api-key": API_KEY,
+      },
+    }
+  );
   const data = await response.json();
   res.status(200).json(data);
 }
